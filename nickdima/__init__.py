9c59a3741d777158e7f7110875d0fc75ea1cdbee
Nick Dima <ncd2dq@gmail.com>
2018-07-31 22:00:06 -0400

    fixed route

--- nickdima/__init__.py
+++ nickdima/__init__.py
@@ -51,7 +51,7 @@ def create_app(test_config=None):
     def hayley():
         return "Hi my darling <3"
 
-    @app.route('imupset')
+    @app.route('/imupset')
     def upset():
         return "Ligma"
 
