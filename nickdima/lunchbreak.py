bc6a18dd37fb565c493c6a5f400d4a881a1ba58d
Nick Dima <ncd2dq@gmail.com>
2018-08-06 20:29:48 -0400

    added lunchreak static files

---
+++ nickdima/lunchbreak.py
@@ -0,0 +1,16 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request,
+    session, url_for
+)
+
+from dbmysq import get_db
+
+
+bp = Blueprint('lunchbreak', __name__, url_prefix='/lunchbreak')
+
+
+@bp.route('/')
+def home():
+
+
+    return render_template('lunchbreak/index.html')
