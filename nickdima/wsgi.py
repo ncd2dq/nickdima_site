e4ef31413968c519f8115c0a51a6f9115e50f116
Nick Dima <ncd2dq@gmail.com>
2018-09-05 18:32:25 -0400

    changed wsgi

--- nickdima/wsgi.py
+++ nickdima/wsgi.py
@@ -4,6 +4,5 @@ sys.path.insert(0, "/app/nickdima")
 
 from __init__ import create_app, socker
 
-if __name__ == '__main__':
-    app = create_app()
-    socker.run(app)
+app = create_app()
+socker.run(app)
