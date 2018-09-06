80788b73ec0893f258b0f0fa23e5bdc7a31cdaa1
Nick Dima <ncd2dq@gmail.com>
2018-09-06 19:54:05 -0400

    chagned speed

--- nickdima/__init__.py
+++ nickdima/__init__.py
@@ -147,7 +147,8 @@ def create_app(test_config=None):
         if ball['y'] > 400 or ball['y'] < 0:
             ball['y_s'] *= - 1
         elif ball['x'] > 400 or ball['x'] < 0:
-            ball = {'x': 200, 'y': 200, 'x_s': 6, 'y_s': 6}
+            #ball = {'x': 200, 'y': 200, 'x_s': 6, 'y_s': 6}
+            ball['x_s'] *= -1
 
         #determine if ball hits paddles
         key_list = db.keys()
