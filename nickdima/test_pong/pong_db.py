370d30fd3c0f1fab948765025ed0b0c6c8f3f6f9
Nick Dima <ncd2dq@gmail.com>
2018-09-06 23:04:11 -0400

    typo in player score

--- nickdima/test_pong/pong_db.py
+++ nickdima/test_pong/pong_db.py
@@ -1,5 +1,5 @@
 db = {'count': 0}
-ball_location = {'x': 200, 'y': 200, 'x_s': 5, 'y_s': 5, 'player_1_socre': 0, 'player_2_score': 0}
+ball_location = {'x': 200, 'y': 200, 'x_s': 5, 'y_s': 5, 'player_1_score': 0, 'player_2_score': 0}
 
 def get_db():
     global db
@@ -15,3 +15,4 @@ def restart_db():
     global db 
     
     db = {'count': 0}
+    ball_location = {'x': 200, 'y': 200, 'x_s': 5, 'y_s': 5, 'player_1_score': 0, 'player_2_score': 0}
