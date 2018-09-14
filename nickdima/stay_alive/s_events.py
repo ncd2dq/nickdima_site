25507f77d435ea4b3627b080e5c621cafea612d0
Nick Dima <ncd2dq@gmail.com>
2018-09-13 21:47:33 -0400

    changed connect methods

--- nickdima/stay_alive/s_events.py
+++ nickdima/stay_alive/s_events.py
@@ -2,7 +2,7 @@ from nickdima.socker import socker
 from stay_alive.main_logic import run_survivor
 
 
-@socker.on('connect', namespace='/stay_alive')
+@socker.on('connect')
 def handle_connect():
     print('Connected to stay_alive')
-    socker.emit('con_test', {'stay alive': 'complete'})
+    socker.emit('con_test', {'stay alive': 'from stay alive'})
