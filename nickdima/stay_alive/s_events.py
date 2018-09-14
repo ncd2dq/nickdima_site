b50c0c4351eff7087df2616b1638365534daf0c1
Nick Dima <ncd2dq@gmail.com>
2018-09-13 21:05:55 -0400

    namespacing

--- nickdima/stay_alive/s_events.py
+++ nickdima/stay_alive/s_events.py
@@ -1,7 +1,7 @@
 from nickdima.socker import socker
 from stay_alive import run_survivor
 
-@socker.on('connect')
+@socker.on('connect', namespace='/stay_alive')
 def handle_connect():
     print('Connected to stay_alive')
     socker.emit('con_test', {'stay alive': 'complete'})
