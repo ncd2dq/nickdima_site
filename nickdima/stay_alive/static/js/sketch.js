7799e0cb0515b0a71d6667e941aabf043aa6571d
Nick Dima <ncd2dq@gmail.com>
2018-09-15 20:32:43 -0400

    changed hero color

--- nickdima/stay_alive/static/js/sketch.js
+++ nickdima/stay_alive/static/js/sketch.js
@@ -6,6 +6,7 @@ function setup(){
 function draw(){
 	background(0, 0, 0);
 
+	fill(255, 255, 255)
 	if(hero_data){
 		for(let i = 0; i < hero_data.length; i++){
 			ellipse(hero_data[i]['location'][0], hero_data[i]['location'][1], hero_data[i]['hitbox']['x_len'], hero_data[i]['hitbox']['y_len']);
