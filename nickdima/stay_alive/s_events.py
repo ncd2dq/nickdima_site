b37e5495f549167270155f37a72673f7a48985ea
Nick Dima <ncd2dq@gmail.com>
2018-09-15 23:18:48 -0400

    allow player disconnect without ending game

--- nickdima/stay_alive/s_events.py
+++ nickdima/stay_alive/s_events.py
@@ -56,10 +56,8 @@ def handle_mov_req(data):
 def handle_disconnect():
     global survivor_thread
     db = get_db()
-    print(db, 'pre reset')
-    survivor_thread.kill()
-    reset_db()
-
-
-    db = get_db()
-    print(db, 'post reset')
+    if db['total_players'] == 1:
+        survivor_thread.kill()
+        reset_db()
+    else:
+        db['total_players'] -= 1
