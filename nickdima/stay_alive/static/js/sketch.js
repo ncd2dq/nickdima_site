2d0eeccfd325e1a650619cbe2a0574f5e7678aec
Nick Dima <ncd2dq@gmail.com>
2018-09-16 14:21:23 -0400

    attempted relative movement

--- nickdima/stay_alive/static/js/sketch.js
+++ nickdima/stay_alive/static/js/sketch.js
@@ -1,15 +1,34 @@
+let vision_x_max = 400;
+let vision_y_max = 400;
+let player_pos_x = 200;
+let player_pos_y = 200;
+let player_actual_x;
+let player_actual_y;
+
 function setup(){
-	let canvas = createCanvas(400, 400);
+	let canvas = createCanvas(vision_x_max, vision_y_max);
 	canvas.parent('sketch-holder');
 }
 
 function draw(){
 	background(0, 0, 0);
 
+	//Draw your hero in the center and other heros as relative to you
 	fill(255, 255, 255);
 	if(hero_data){
 		for(let i = 0; i < hero_data.length; i++){
-			rect(hero_data[i]['location'][0], hero_data[i]['location'][1], hero_data[i]['hitbox']['x_len'], hero_data[i]['hitbox']['y_len']);
+			if(hero_data[i]['id'] == my_hero_id){
+				player_actual_x = hero_data[i]['location'][0];
+				player_actual_y = hero_data[i]['location'][1];
+				rect(player_pos_x, player_pos_y, hero_data[i]['hitbox']['x_len'], hero_data[i]['hitbox']['y_len']);
+			} else {
+				//Determine the relative distance between your hero's actual and the other hero
+				let rel_x = player_actual_x - hero_data[i]['location'][0];
+				let rel_y = player_actual_y - hero_data[i]['location'][1];
+
+				//Apply the relative x/y translation to where your hero is fixed
+				rect(player_pos_x - rel_x, player_pos_y - rel_y, hero_data[i]['hitbox']['x_len'], hero_data[i]['hitbox']['y_len']);
+			} 
 		}
 	}
 }
