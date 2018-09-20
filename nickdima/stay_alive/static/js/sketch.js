1f4b30b0567986c1c8c2d311d86c11d9ba9afbaf
Nick Dima <ncd2dq@gmail.com>
2018-09-19 22:08:16 -0400

    chaged bar

--- nickdima/stay_alive/static/js/sketch.js
+++ nickdima/stay_alive/static/js/sketch.js
@@ -67,9 +67,9 @@ function draw(){
 
 	if(hero_data){
 		fill(255, 255, 255);
-		rect(10, 10, vision_x_max - 10, 10);
+		rect(10, 10, vision_x_max - 20, 10);
 		fill(0, 0, 0);
-		rect(10, 10, daylight_data['current'] / daylight_data['maxi'] * (vision_x_max - 10) , 10);
+		rect(10, 10, daylight_data['current'] / daylight_data['maxi'] * (vision_x_max - 20) , 10);
 	}
 }
 
