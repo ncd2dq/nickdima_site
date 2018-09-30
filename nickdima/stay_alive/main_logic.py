99998174c27f73be174055374f89d909c1c6d873
Nick Dima <ncd2dq@gmail.com>
2018-09-30 16:49:26 -0400

    fixed if statement

--- nickdima/stay_alive/main_logic.py
+++ nickdima/stay_alive/main_logic.py
@@ -72,8 +72,8 @@ def game_loop():
 
     # Remove depleated resources
     for resource_node in db['resource_nodes']:
-        if resource_node.amount == 0
-        db['resource_nodes'].remove(resource_node)
+        if resource_node.amount == 0:
+            db['resource_nodes'].remove(resource_node)
     # 10% chance of a new resource node as long as there are less than 15 on the map
     if random.random() < 0.1 and len(db['resource_nodes']) < 15:
         db['resource_nodes'].append(
