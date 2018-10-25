97e7ccaed8d9f439cabeaafd7d3479a4c206c962
Nick Dima <ncd2dq@gmail.com>
2018-10-24 22:47:37 -0400

    removed post from menu page

--- nickdima/silk_thai/menu.py
+++ nickdima/silk_thai/menu.py
@@ -2,19 +2,7 @@ from flask import Blueprint, request, render_template, g, session, flash, redire
 
 bp = Blueprint('menu', __name__, url_prefix='/thai/menu', static_folder='static', template_folder='template')
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET'])
 def menu():
-
-    if request.method == 'POST':
-        items = []
-        keys = request.form.keys()
-
-        for key in keys:
-            if request.form[key] != '0':
-                items.append((key, request.form[key]))
-
-        session['cart'] = items
-
-        return redirect(url_for('checkout.summary'))
 
     return render_template('menu/menu.html')
