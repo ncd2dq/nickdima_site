79ded3ed150d3b381050af835e0accb82944d042
Nick Dima <ncd2dq@gmail.com>
2018-11-05 15:52:01 -0500

    automate menu page

--- nickdima/silk_thai/menu.py
+++ nickdima/silk_thai/menu.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, render_template, g, session, flash, redirect, url_for
+from silk_thai.food_db import get_db
 
 bp = Blueprint('menu', __name__, url_prefix='/thai/menu', static_folder='static', template_folder='template')
 
 @bp.route('/', methods=['GET'])
 def menu():
+    db = get_db()
     session['from_summary'] = False
-    return render_template('menu/menu.html')
+    return render_template('menu/menu.html', all_items_dict=db)
